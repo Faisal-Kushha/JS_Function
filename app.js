11de765d013cf03ba0721bf1246f91db907aab9b
Faisal <fkushha89@hotmail.com>
2023-09-30 17:05:47 +0300

    New feature

--- app.js
+++ app.js
@@ -2,25 +2,58 @@
 
 let student = ["ahmad", "omar", "enas", "ghadeer", "farah", "eman"];
 
-function myFunction(x) {
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let x = letters[Math.floor(Math.random() * letters.length)];
+console.log(x);
+
+function myFunction() {
   for (let i = 0; i < student.length; i++) {
     if (student[i][0] == x) {
-      console.log(student[i]);
+      console.log(`This name ${student[i]} start with the letter ${x}`);
     } else {
       console.log("Sorry this name does not start with your letter");
     }
   }
 }
 
-let x = myFunction("o");
+myFunction();
 
-let myFunc = (z) => {
+let myFunc = () => {
   for (let i = 0; i < student.length; i++) {
-    if (student[i][0] == z) {
-      console.log(student[i]);
+    if (student[i][0] == x) {
+      console.log(`This name ${student[i]} start with the letter ${x}`);
     } else {
       console.log("Sorry this name does not start with your letter");
     }
   }
 };
-let z = myFunc("o");
+
+myFunc();
